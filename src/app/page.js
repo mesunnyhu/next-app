63de85bc9e5c0a4b80f3c5d49344a15f00e71d7d
mesunnyhu <mrsunnysharma99@gmail.com>
2024-03-28 13:52:34 +0530

    paged

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
+import Navbar from "@/components/Header/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16 bg-zinc-900">
-      <div className="text-5xl font-serif antialiased ">
-        hello
-      </div>
+    <main data-theme="sunset" className="flex min-h-screen flex-col items-center justify-between">
+      <Navbar />
     </main>
   );
 }
