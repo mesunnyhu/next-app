0c3825b8bb29d48662bd572f273bf52862ba7684
mesunnyhu <mrsunnysharma99@gmail.com>
2024-03-28 14:06:15 +0530

    navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main data-theme="sunset" className="flex min-h-screen flex-col items-center justify-between">
+    <main data-theme="wireframe" className="flex min-h-screen flex-col items-center justify-between">
       <Navbar />
     </main>
   );
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -25,15 +25,18 @@ function Navbar() {
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-          </ul>
+          ></ul>
         </div>
-        <Link href={'/'} className="btn btn-ghost text-xl">daisyUI</Link>
+        <Link href={"/"} className="btn btn-ghost text-xl">
+          daisyUI
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <Link className="" href={'/'}>Home</Link>
+            <Link className="" href={"/"}>
+              Home
+            </Link>
           </li>
           <li>
             <details>
@@ -54,7 +57,37 @@ function Navbar() {
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
